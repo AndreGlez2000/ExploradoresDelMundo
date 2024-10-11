759cf1c32b81402502031b7b5496f0d24e8e6709
AndreGlez2000 <gonzalez.andre@uabc.edu.mx>
2024-10-10 20:25:03 -0700

    A ver si asi sirve el cabron

---
+++ src/Quiz.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import java.awt.*;
+import java.swing.*;
+
+public class Quiz implements ActionListener{
+
+    public Quiz(){
+
+    }
+
+    public void nextQuestion() {
+
+    }
+
+
+}
